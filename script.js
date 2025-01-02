7121c732f181c077f2b9e01287f386fd0394e44f
Kaan <103496597+RoyalNegative@users.noreply.github.com>
2025-01-02 03:02:45 +0300

    added the normal link for api fetch

--- script.js
+++ script.js
@@ -4,20 +4,16 @@ const apiUrl = "https://pracc-0dcdba945892.herokuapp.com"; // Backend base URL
 fetch(apiUrl)
   .then(response => response.text()) // Use text() as backend sends plain text
   .then(data => {
-    console.log("Root Endpoint Data:", data);
+    if(data === ""){
+      return; // No need to log empty response
+    }
+    else{
+      console.log("Root Endpoint Data:", data);
+      document.getElementById("text").textContent = data;
+    }
   })
   .catch(error => console.error("Error fetching root endpoint:", error));
 
-// Fetch the /api/message endpoint
-const text = document.getElementById("text");
 
-fetch("/api/message") 
-  .then(response => response.text()) // Use text() since backend sends plain text
-  .then(data => {
-    text.innerText = data; // Update the HTML element with the response text
-  })
-  .catch(error => {
-    console.error("Error fetching /api/message:", error);
-    text.innerText = "Failed to load message."; // Display error message in the UI
-  });
+
 
